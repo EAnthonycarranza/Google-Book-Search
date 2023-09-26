f4568ec250f6844df8641460c3c1d6d1a1b0ed92
EAnthonycarranza <129874785+EAnthonycarranza@users.noreply.github.com>
2023-09-25 22:30:47 -0500

    Add files via upload

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,49 @@
+const { gql } = require('apollo-server-express');
+
+// Type Definitions for Queries and Mutations
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+    }
+
+    input BookInput {
+        bookId: String!
+        authors: [String]
+        description: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: BookInput): User
+        removeBook(bookId: String!): User
+      }
+
+`;
+
+module.exports = typeDefs;
